16b73094a01c56a7623deb62e124a4fde7f7062d
Łukasz Niedziałek <intergenixyt@gmail.com>
2017-04-19 21:28:41 +0200

    i have changed the form of code, so now it is more readable

--- app/src/main/java/com/example/genix/calculator/MainActivity.java
+++ app/src/main/java/com/example/genix/calculator/MainActivity.java
@@ -1,81 +1,44 @@
 package com.example.genix.calculator;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     static double result = 0;
+    TextView mainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainText = (TextView) findViewById(R.id.mainView);  //TO REMEMBER
     }
 
     public void write(View view) {
-
-        TextView mainText = (TextView) findViewById(R.id.mainView);
-
         Button tempBut = (Button) view;
+
         mainText.setText(mainText.getText() + tempBut.getText().toString());
 
     }
 
     public void clear(View view) {
-        TextView mainText = (TextView) findViewById(R.id.mainView);
         mainText.setText("");
         result = 0;
     }
 
+    //Change value of result and give it to TextView
     public void getResult(View view) {
+        MathFunctions functions = new MathFunctions();
 
-        TextView mainText = (TextView) findViewById(R.id.mainView);
-        calculations();
+        functions.calculations(mainText.getText().toString());
         DecimalFormat df = new DecimalFormat("#.#####");
         mainText.setText(String.valueOf(df.format(result)));
-
-    }
-
-    public void calculations(){
-
-
-        TextView mainText = (TextView) findViewById(R.id.mainView);
-        String string = mainText.getText().toString();
-        char[] ourArr = string.toCharArray();
-
-        String number1 = "";
-        String number2 = "";
-
-        char sign = 'a';
-
-        for(int i = 0 ; i < ourArr.length ; ++i){
-            if(Character.isDigit(ourArr[i]) || ourArr[i] == '.') {
-                if (sign == 'a') {
-                    number1 += ourArr[i];
-                } else
-                    number2 += ourArr[i];
-            } else if(ourArr[i] != ' '){
-                sign = ourArr[i];
-            }
-        }
-
-        switch (sign){
-            case '+': result = Double.parseDouble(number1) + Double.parseDouble(number2);
-                break;
-            case '-': result = Double.parseDouble(number1) - Double.parseDouble(number2);
-                break;
-            case '/': result = Double.parseDouble(number1) / Double.parseDouble(number2);
-                break;
-            case '*': result = Double.parseDouble(number1) * Double.parseDouble(number2);
-                break;
-        }
-
     }
 
 }
---
+++ app/src/main/java/com/example/genix/calculator/MathFunctions.java
@@ -0,0 +1,55 @@
+package com.example.genix.calculator;
+
+
+ class MathFunctions {
+
+    private double number1;
+    private double number2;
+    private char sign = 'a';
+
+    //Assign values of variables
+    private void setVariables(String textViewOutput){
+
+        char[] ourArr = textViewOutput.toCharArray();
+
+        String number1 = "";
+        String number2 = "";
+
+        sign = 'a';
+
+        for(int i = 0 ; i < ourArr.length ; ++i){
+            if(Character.isDigit(ourArr[i]) || ourArr[i] == '.') {
+                if (sign == 'a') {
+                    number1 += ourArr[i];
+                } else
+                    number2 += ourArr[i];
+            } else if(ourArr[i] != ' '){
+                sign = ourArr[i];
+            }
+        }
+
+        this.number1 = Double.parseDouble(number1);
+        this.number2 = Double.parseDouble(number2);
+
+
+    }
+
+    //Change value of result(static from MainActivity)
+    void calculations(String textViewOutput){
+
+        setVariables(textViewOutput);
+
+        switch (sign){
+            case '+': MainActivity.result = number1 + number2;
+                break;
+            case '-': MainActivity.result = number1 - number2;
+                break;
+            case '/': MainActivity.result = number1 / number2;
+                break;
+            case '*': MainActivity.result = number1 * number2;
+                break;
+        }
+
+    }
+
+}
